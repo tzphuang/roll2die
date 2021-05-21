f03b85717bd5bdb5c0969413cec68cc149d5c178
Tony <thuang54@mail.ccsf.edu>
2021-05-20 20:08:47 -0700

    still trying to fix jar

--- src/Roll2Die/GameConstants.java
+++ src/Roll2Die/GameConstants.java
@@ -11,7 +11,7 @@ public class GameConstants {
     public static final int CONTROLS_MENU_SCREEN_HEIGHT = 700;
 
     public static final int OVERWORLD_SCREEN_WIDTH = 1400;
-    public static final int OVERWORLD_SCREEN_HEIGHT = 1000;
+    public static final int OVERWORLD_SCREEN_HEIGHT = 1080;
 
     public static final int OVERWORLD_HUD_WIDTH = 1400;
     public static final int OVERWORLD_HUD_PLAYER_MAIN_STATS_WIDTH = 300;
@@ -20,7 +20,7 @@ public class GameConstants {
     public static final int OVERWORLD_HUD_HEIGHT = 200;
 
     public static final int GAME_SCREEN_WIDTH = 1400;
-    public static final int GAME_SCREEN_HEIGHT = 1080;
+    public static final int GAME_SCREEN_HEIGHT = 1000;
 
     public static final int WINNER_SCREEN_WIDTH = 500;
     public static final int WINNER_SCREEN_HEIGHT = 700;
